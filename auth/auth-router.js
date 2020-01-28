480950e140b3395dff63a3d9ffb98c38f302265f
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-01-28 11:38:39 +0100

    wip

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -21,10 +21,18 @@ router.post('/register', (req, res) => {
 router.post('/login', (req, res) => {
   let { username, password } = req.body;
 
+  // thanks to express-session
+  // when we hit this enpdoint
+  // a session gets created on the server
+  // if credentials valid, as per bcrypt
+  // we will add something to the session object
+  // and send back a welcome
+
   Users.findBy({ username })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.loggedInUser = user;
         res.status(200).json({
           message: `Welcome ${user.username}!`,
         });
